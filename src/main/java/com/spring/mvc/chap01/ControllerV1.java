d2e41ffbe3eaa1bba8c2f8deea4ac7418218b7d5
ejdrp1 <ejdrp1@naver.com>
2023-12-12 15:18:53 +0900

    feat(ControllerV1) : @RequestParam 사용법

--- src/main/java/com/spring/mvc/chap01/ControllerV1.java
+++ src/main/java/com/spring/mvc/chap01/ControllerV1.java
@@ -1,7 +1,9 @@
 package com.spring.mvc.chap01;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 //  어떤 요청들을 처리할지 공통 URL 을 설계
 @RequestMapping("/spring/*")
@@ -23,6 +25,42 @@ public class ControllerV1 {
         System.out.println("\n===== hello 요청이 들어옴!! =====\n");
         return "chap01/food";
     }
+
+//    =========== 요청 파라미터 읽기 (Query String parameter) ===========
+//    == 1. HttpServletRequest 사용하기
+//    ==> ex) /spring/person?name=kim&age=30
+
+    @RequestMapping("/person")
+    public String person(HttpServletRequest request) {
+        String name = request.getParameter("name");
+        String age = request.getParameter("age");
+
+        System.out.println("name : " + name);
+        System.out.println("age : " + age);
+
+        return "";
+    }
+
+//    == 2. @RequestParam 사용하기
+//    ==> ex) /spring/major?stu=kim&major=business&grade=3
+    @RequestMapping("/major")
+    public String major(
+            String stu,
+            @RequestParam("major") String mj,
+            @RequestParam(defaultValue = "1") int grade
+    ) {
+
+        String major = "전공전공";
+
+        System.out.println("stu : " + stu);
+        System.out.println("major : " + mj);
+        System.out.println("grade : " + grade);
+        return "";
+    }
+
+//    == 3. 커멘드 객체 사용
+
+
 
 
 }
