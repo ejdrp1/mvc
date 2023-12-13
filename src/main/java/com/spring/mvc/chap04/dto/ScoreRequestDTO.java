ea448f88ae23d96e48d1d12204ce0e871d2b1b50
ejdrp1 <ejdrp1@naver.com>
2023-12-13 13:38:09 +0900

    feat(ScoreRequestDTO) : 클래스 생성

---
+++ src/main/java/com/spring/mvc/chap04/dto/ScoreRequestDTO.java
@@ -0,0 +1,10 @@
+package com.spring.mvc.chap04.dto;
+
+public class ScoreRequestDTO {
+
+    //    DTO 는 클라이언트가 전달한 데이터만 가지고 있음
+    private String name; // 학생 이름
+    private int kor, eng, math; // 국, 영, 수 점수
+
+
+}
--- src/main/java/com/spring/mvc/chap04/entity/Score.java
+++ src/main/java/com/spring/mvc/chap04/entity/Score.java
@@ -11,8 +11,6 @@ import lombok.*;
 @AllArgsConstructor
 public class Score {
 
-    private String name; // 학생 이름
-    private int kor, eng, math; // 국, 영, 수 점수
     private int stuNum; // 학번
     private int total; // 총점
     private double average; // 평균
