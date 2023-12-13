3240b39d1e9e1e91dd1d93b0b35ed711d84345a4
ejdrp1 <ejdrp1@naver.com>
2023-12-13 09:58:57 +0900

    feat(ScoreRepositoryImpl) : 임시 데이터 생성

--- src/main/java/com/spring/mvc/chap04/repository/ScoreRepositoryImpl.java
+++ src/main/java/com/spring/mvc/chap04/repository/ScoreRepositoryImpl.java
@@ -1,4 +1,49 @@
 package com.spring.mvc.chap04.repository;
 
-public class ScoreRepositoryImpl {
+import com.spring.mvc.chap04.entity.Score;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static com.spring.mvc.chap04.entity.Grade.A;
+
+public class ScoreRepositoryImpl implements ScoreRepository{
+
+//    key : 학번, value : 성적 정보
+    private static final Map<Integer, Score> scoreMap;
+
+//    학번에 사용할 일련 번호
+    private static int sequence;
+
+    static {
+        scoreMap = new HashMap<>();
+        Score stu1 = new Score("뽀로로", 100, 50, 70, ++sequence, 0, 0, A);
+        Score stu2 = new Score("춘식이", 33, 56, 12, ++sequence, 0, 0, A);
+        Score stu3 = new Score("대길이", 88, 12, 0, ++sequence, 0, 0, A);
+
+        scoreMap.put(stu1.getStuNum(), stu1);
+        scoreMap.put(stu2.getStuNum(), stu2);
+        scoreMap.put(stu3.getStuNum(), stu3);
+    }
+
+    @Override
+    public List<Score> findAll() {
+        return null;
+    }
+
+    @Override
+    public boolean save(Score score) {
+        return false;
+    }
+
+    @Override
+    public boolean deleteByStuNum(int stuNum) {
+        return false;
+    }
+
+    @Override
+    public Score findByStuNum(int stuNum) {
+        return null;
+    }
 }
